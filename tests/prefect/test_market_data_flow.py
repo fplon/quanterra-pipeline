40f57404841059baf8733e8d16b2e4e59d5001c1
Finlay <fploneill@gmail.com>
2025-02-12 21:10:15 +0000

    🔥 Rm redundant market_data flow
    Replaced by the individual data source ingestion flows

--- tests/prefect/test_market_data_flow.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from src.flows.market_data import run_market_data_ingestion
-
-
-@pytest.mark.asyncio
-async def test_market_data_flow() -> None:
-    await run_market_data_ingestion()
