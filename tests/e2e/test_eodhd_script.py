8694f21894806f3d29730b9e6e72ffc0988e0e1c
Finlay <fploneill@gmail.com>
2025-02-13 06:46:48 +0000

    ⚰️ Rm redundant code - pre-Prefect

--- tests/e2e/test_eodhd_script.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-from src.scripts.ingest.eodhd import run_eodhd_ingestion
-
-
-def test_eodhd_ingestion_script() -> None:
-    asyncio.run(run_eodhd_ingestion())
-    assert True
--- tests/e2e/test_yahoo_finance_script.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-from src.scripts.ingest.yahoo_finance import run_yahoo_finance_ingestion
-
-
-def test_yahoo_finance_ingestion_script() -> None:
-    asyncio.run(run_yahoo_finance_ingestion())
-    assert True
